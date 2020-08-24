c95954f4e27b0ba309836d96ef5c4095f5670a1e
Roey Benamotz <roey@benamotz.com>
2020-08-24 16:20:13 -0400

    blink support

--- views/led_view.py
+++ views/led_view.py
@@ -1,4 +1,7 @@
 import RPi.GPIO as GPIO
+import threading
+import time
+
 
 import application
 from views.view import View
@@ -18,19 +21,30 @@ class LedsView(View):
         GPIO.setwarnings(False)
         for p in ALL_PINS:
             GPIO.setup(p, GPIO.OUT)
+        self.blink_state = False
+        blinker = threading.Thread(target=self.blink, daemon=True)
+        blinker.start()
+
+    def blink(self):
+        while True:
+            self.refresh()
+            time.sleep(0.1)
+            self.blink_state = not self.blink_state
 
     def __update_blue(self):
+        if self.app.app_state == application.Application.APP_STATE_PROGRAMMING:
+            self.blink_gpio = PIN_BLUE
         b = False
-        b = b or (self.app.app_state ==
-                  application.Application.APP_STATE_WAITING_FOR_BUTTON)
+        b = b or (self.app.app_state == application.Application.APP_STATE_WAITING_FOR_BUTTON)
         b = b or self.app.blue_led_on
+        b = b or (self.blink_state and self.app.app_state == application.Application.APP_STATE_PROGRAMMING)
         GPIO.output(PIN_BLUE, b)
 
     def __update_green(self):
         b = False
-        b = b or (self.app.app_state ==
-                  application.Application.APP_STATE_SUCCESS)
+        b = b or (self.app.app_state == application.Application.APP_STATE_SUCCESS)
         b = b or self.app.green_led_on
+        b = b or (self.blink_gpio == PIN_GREEN and self.blink_state)
         GPIO.output(PIN_GREEN, b)
 
     def __update_red(self):
@@ -44,6 +58,7 @@ class LedsView(View):
         GPIO.output(PIN_BLUE2, b)
 
     def refresh(self):
+        self.blink_gpio = -1
         self.__update_blue()
         self.__update_green()
         self.__update_red()
