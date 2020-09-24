1066fcde112638d293ad17292822d3efa133b670
Roey Benamotz <roey@benamotz.com>
2020-09-24 08:42:17 -0400

    Refactoring

---
+++ states/state_factory.py
@@ -0,0 +1,32 @@
+from application import Application
+from .env_init_state import EnvInit
+from .fail_state import FailState
+from .firmware_downloader import FirmwareDownload
+from .program_state import ProgramState
+from .sense_profile_state import SensingProfileState
+from .success_state import SuccessState
+from .wait_for_button_state import WaitForButtonState
+from .fw_erase import FirmwareEraseState
+from .exception_state import ExceptionState
+
+
+def state_by_code(code, app):
+    if code == Application.APP_STATE_INIT:
+        return EnvInit(app)
+    if code == Application.APP_STATE_PROFILE_SENSE:
+        return SensingProfileState(app)
+    if code == Application.APP_STATE_FIRMWARE_DOWNLOAD:
+        return FirmwareDownload(app)
+    if code == Application.APP_STATE_WAITING_FOR_BUTTON:
+        return WaitForButtonState(app)
+    if code == Application.APP_STATE_PROGRAMMING:
+        return ProgramState(app)
+    if code == Application.APP_STATE_SUCCESS:
+        return SuccessState(app)
+    if code == Application.APP_STATE_FAIL:
+        return FailState(app)
+    if code == Application.APP_STATE_ERASE:
+        return FirmwareEraseState(app)
+    if code == Application.APP_STATE_EXCEPTION:
+        return ExceptionState(app)
+    raise Exception("Unknown state %s" % code)
