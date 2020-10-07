f1abf495489f9538379d9fbe83ba705c45a1d511
Roey Benamotz <roey@benamotz.com>
2020-10-06 22:10:28 -0400

    Filesystem based copy

--- states/binfetchers/binfetcher_factory.py
+++ states/binfetchers/binfetcher_factory.py
@@ -1,9 +1,14 @@
 from .url_fetcher import UrlFetcher, CloudGwFetcher
+from .fs_fetcher import LocalFetcher, FSCopyFetcher
 
 def create_fetcher(method, app):
     if method == "cloud":
         return UrlFetcher(app)
     if method == "cloud_gw":
         return CloudGwFetcher(app)
+    if method == "local":
+        return LocalFetcher(app)
+    if method == "fs":
+        return FSCopyFetcher(app)
     raise Exception("No fetcher for method {}".format(method))
 
