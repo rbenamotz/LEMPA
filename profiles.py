4bff3a892ed4dcaeff6a6b1d4c3d5b0b1a0d8a84
Roey Benamotz <roey@benamotz.com>
2021-04-13 14:20:54 -0400

    secutiy patch

--- profiles.py
+++ profiles.py
@@ -7,19 +7,23 @@ profile_data = None
 
 def init_profile_data(app):
     global profile_data
+    file_name = "profiles.json"
+    if len(sys.argv) >= COMMAND_LINE_PARAM_CONFIG_FILE + 1:
+        p = sys.argv[COMMAND_LINE_PARAM_CONFIG_FILE]
+        if (p.startswith("http")):
+            app.profiles_url = p
+        else:
+            file_name = p
     if (app.profiles_url):
         app.detail("Loding profiles from " + app.profiles_url)
         with urllib.request.urlopen(app.profiles_url) as url:
             profile_data = json.loads(url.read().decode())
         return
     app.detail("Loading local profiles")
-    file_name = "profiles.json"
     if len(sys.argv) >= COMMAND_LINE_PARAM_CONFIG_FILE + 1:
         file_name = sys.argv[COMMAND_LINE_PARAM_CONFIG_FILE]
     with open(file_name) as f:
         profile_data = json.load(f)
-    
-
 
 def profile_by_jumper(jumper):
     for x in profile_data:
